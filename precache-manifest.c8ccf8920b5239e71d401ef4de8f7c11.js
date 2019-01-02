279297513b5e181f4326021c73d1aff42b383818
mariam <mariam@gmail.com>
2019-01-02 06:21:06 +0200

    Updates

---
+++ precache-manifest.c8ccf8920b5239e71d401ef4de8f7c11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1894f7852d18ec5d304b38c61e5a8b55",
+    "url": "/foodmenu/static/media/header_backgroung.1894f785.jpg"
+  },
+  {
+    "revision": "fd01b5c40606c63a704aaf38dd865d81",
+    "url": "/foodmenu/static/media/burger.fd01b5c4.jpg"
+  },
+  {
+    "revision": "0baa93cb71af13c243ad",
+    "url": "/foodmenu/static/js/runtime~main.0baa93cb.js"
+  },
+  {
+    "revision": "0f1910011c2897521a58",
+    "url": "/foodmenu/static/js/main.0f191001.chunk.js"
+  },
+  {
+    "revision": "8ec7636ebbdd1185797c",
+    "url": "/foodmenu/static/js/1.8ec7636e.chunk.js"
+  },
+  {
+    "revision": "0f1910011c2897521a58",
+    "url": "/foodmenu/static/css/main.c8ad9dae.chunk.css"
+  },
+  {
+    "revision": "b95df66af935d6d71b85bef15cf94b85",
+    "url": "/foodmenu/index.html"
+  }
+];
